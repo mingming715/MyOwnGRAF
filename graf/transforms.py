84c44ffe1ac3fbe7972a3a0bb8894fc607688f8a
Kang Minseok <richkang715@gmail.com>
2023-02-04 14:08:19 +0900

    230204 14:08 Update

--- graf/transforms.py
+++ graf/transforms.py
@@ -49,3 +49,78 @@ class FullRaySampler(RaySampler):
 
     def sample_rays(self, H, W):
         return torch.arange(0, H*W)
+
+        
+
+class ImgToPatch(object):
+    def __init__(self, ray_sampler, hwf):
+        self.ray_sampler = ray_sampler
+        self.hwf = hwf      # camera intrinsics
+
+    def __call__(self, img):
+        rgbs = []
+        for img_i in img:
+            pose = torch.eye(4)         # use dummy pose to infer pixel values
+            _, selected_idcs, pixels_i = self.ray_sampler(H=self.hwf[0], W=self.hwf[1], focal=self.hwf[2], pose=pose)
+            if selected_idcs is not None:
+                rgbs_i = img_i.flatten(1, 2).t()[selected_idcs]
+            else:
+                rgbs_i = torch.nn.functional.grid_sample(img_i.unsqueeze(0), 
+                                     pixels_i.unsqueeze(0), mode='bilinear', align_corners=True)[0]
+                rgbs_i = rgbs_i.flatten(1, 2).t()
+            rgbs.append(rgbs_i)
+
+        rgbs = torch.cat(rgbs, dim=0)       # (B*N)x3
+
+        return rgbs
+
+class FlexGridRaySampler(RaySampler):
+    def __init__(self, N_samples, random_shift=True, random_scale=True, min_scale=0.25, max_scale=1., scale_anneal=-1,
+                 **kwargs):
+        self.N_samples_sqrt = int(sqrt(N_samples))
+        super(FlexGridRaySampler, self).__init__(self.N_samples_sqrt**2, **kwargs)
+
+        self.random_shift = random_shift
+        self.random_scale = random_scale
+
+        self.min_scale = min_scale
+        self.max_scale = max_scale
+
+        # nn.functional.grid_sample grid value range in [-1,1]
+        self.w, self.h = torch.meshgrid([torch.linspace(-1,1,self.N_samples_sqrt),
+                                         torch.linspace(-1,1,self.N_samples_sqrt)])
+        self.h = self.h.unsqueeze(2)
+        self.w = self.w.unsqueeze(2)
+
+        # directly return grid for grid_sample
+        self.return_indices = False
+
+        self.iterations = 0
+        self.scale_anneal = scale_anneal
+
+    def sample_rays(self, H, W):
+
+        if self.scale_anneal>0:
+            k_iter = self.iterations // 1000 * 3
+            min_scale = max(self.min_scale, self.max_scale * exp(-k_iter*self.scale_anneal))
+            min_scale = min(0.9, min_scale)
+        else:
+            min_scale = self.min_scale
+
+        scale = 1
+        if self.random_scale:
+            scale = torch.Tensor(1).uniform_(min_scale, self.max_scale)
+            h = self.h * scale 
+            w = self.w * scale 
+
+        if self.random_shift:
+            max_offset = 1-scale.item()
+            h_offset = torch.Tensor(1).uniform_(0, max_offset) * (torch.randint(2,(1,)).float()-0.5)*2
+            w_offset = torch.Tensor(1).uniform_(0, max_offset) * (torch.randint(2,(1,)).float()-0.5)*2
+
+            h += h_offset
+            w += w_offset
+
+        self.scale = scale
+
+        return torch.cat([h, w], dim=2)
